ec73b3696991733e611b5b912953c91c486be241
james.davis <james.davis@atsid.com>
2013-11-11 14:56:39 -0800

    Adding schema service

--- app/main.js
+++ app/main.js
@@ -9,7 +9,8 @@ if (process.env.license) {
 var express = require('express')
     , http = require('http')
     , path = require('path')
-    , autorest = require('./autorest');
+    , autorest = require('./autorest')
+    , schemaService = require('./schema-service');
 
 var app = express();
 
@@ -25,6 +26,10 @@ app.use(express.static(__dirname));
 autorest(app, {
     middleware: [express.bodyParser()]
 }).scan();
+
+schemaService(app, {
+    middleware: [express.bodyParser()]
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
