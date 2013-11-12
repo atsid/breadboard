40c5138411f66ff21bac18d5fed920b879d99c29
talkswithnumbers <cloudbees@talkswithnumbers.com>
2013-11-11 21:03:34 -0800

    added additional schema service methods for rel and rql

--- app/schema-service.js
+++ app/schema-service.js
@@ -4,4 +4,10 @@ module.exports = function (app, config) {
     app.get('/schema/models/:model', config.middleware, function (req, res, next) {
         res.status(200).sendfile("schema/models/" + req.params.model + ".json");
     });
+    app.get('/schema/rel/:rel', config.middleware, function (req, res, next) {
+        res.status(200).sendfile("schema/rel/" + req.params.rel + ".txt");
+    });
+    app.get('/schema/rql/:rql', config.middleware, function (req, res, next) {
+        res.status(200).sendfile("schema/rql/" + req.params.rql + ".rql");
+    });
 }
