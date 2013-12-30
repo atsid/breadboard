351e6b6aeb9167ca9d556f718fd853900dff4f92
Brian Mathews <spoonart@gmail.com>
2013-12-30 11:05:49 -0800

    adding 'grunt start' and 'grunt start:brk' to automatically enable node-inspector

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -22,12 +22,30 @@ module.exports = function (grunt) {
                 files: ['**/*.js', '!**/node_modules/**'],
                 tasks: ['lint', 'test']
             }
+        },
+        'node-inspector': {
+            dev: { 
+                "debug-port": 3000,
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-node-inspector');
+
+    // start server using debug flag and start node-inspector
+    grunt.registerTask('start', 'Start server and node-inspector', function (brk) {
+        var node = grunt.util.spawn({
+            cmd: 'node',
+            args: [brk ? '--debug-brk' : '--debug', 'main.js']
+        });
+        node.stdout.pipe(process.stdout);
+        node.stderr.pipe(process.stderr);
+
+        grunt.task.run('node-inspector:dev');
+    });
 
     grunt.registerTask('lint', ['jshint']);
     grunt.registerTask('test', ['mochaTest']);
